adb240724ea54ab2b26951a1cb55b62b28a87352
Chanyub Park <mrchypark@gmail.com>
2019-07-30 22:23:14 +0900

    install and check version

--- R/install.R
+++ R/install.R
@@ -1,6 +1,12 @@
 #' @export
 install <- function(){
   dependency(get_os())
-  sys::exec_wait("curl", args = c('-s','"https://get.sdkman.io"','|',"bash"))
+  sys::exec_wait("curl", args = c("-o","install.sh", "https://get.sdkman.io"))
+  sys::exec_wait("bash", args = c("install.sh"))
+  fs::file_delete("install.sh")
 }
 
+#' @export
+sdk_version <- function(){
+  sys::exec_wait("bash", args = c("-c","source $HOME/.sdkman/bin/sdkman-init.sh && sdk version"))
+}
